9c5b82ed04a265ef22923614c5f8b43c636c0d1f
farshad delavarpour <farshaddelavarpour@gmail.com>
2022-02-20 12:49:40 +0330

    develop

--- storefront/urls.py
+++ storefront/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
-
+from rest_framework_swagger.views import get_swagger_view
 admin.site.site_header = 'Storefront Admin'
 admin.site.index_title = 'Admin'
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
